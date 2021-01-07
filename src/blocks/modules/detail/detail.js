bad6bf5b75f3b789220ca1a7a464c5c73933e5e2
georgepostoronca <george.postoronca.work@gmail.com>
2021-01-07 23:13:02 +0200

    init

---
+++ src/blocks/modules/detail/detail.js
@@ -0,0 +1,13 @@
+modules.define('detail', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
