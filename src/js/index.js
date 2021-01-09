5bd6ad853686ba272250b35fd6bc2921344c0983
George <george.postoronca@gmail.com>
2021-01-09 19:47:01 +0300

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,17 @@
 import "./import/modules";
 import "./import/components";
+
+
+// import Swiper JS
+import Swiper, { Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper(".pslider", {
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
