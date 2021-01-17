ed742e030ce210bad71a6b9fb5995e53c0606ddc
George <george.postoronca@gmail.com>
2021-01-17 19:02:19 +0300

    last fix

---
+++ src/blocks/modules/wehelp/wehelp.js
@@ -0,0 +1,13 @@
+modules.define('wehelp', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
